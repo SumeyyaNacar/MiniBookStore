0239acd0d5ba39fc9f12d283701bffed156fb8cc
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-26 00:29:06 +0300

    Create NotebookService.java

---
+++ MiniBookStore/src/NotebookService.java
@@ -0,0 +1,30 @@
+public class NotebookService implements ProductService{
+    //2.Adim : ProductService i implement et ve methodlari notebook a uygun sekilde doldur
+
+
+
+    @Override
+    public void productMenu() {
+
+    }
+
+    @Override
+    public void listProduct() {
+
+    }
+
+    @Override
+    public void addProduct() {
+
+    }
+
+    @Override
+    public void removeProduct() {
+
+    }
+
+    @Override
+    public void filterProduct(String filter) {
+
+    }
+}
