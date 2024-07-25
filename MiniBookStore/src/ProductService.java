1d0f26246d362292fb25701716ccb5f6443e946e
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-26 00:25:52 +0300

    Create ProductService.java

---
+++ MiniBookStore/src/ProductService.java
@@ -0,0 +1,10 @@
+public interface ProductService {
+    //ihtiyacim olan butun productlarin methodlari burada yer alacak
+
+
+    void productMenu();
+    void listProduct();
+    void addProduct();
+    void removeProduct();
+    void filterProduct(String filter);//kullanicidan alicak degere gore listeleme yapilacak
+}
