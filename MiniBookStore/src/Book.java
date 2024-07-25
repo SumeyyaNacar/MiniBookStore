46fda08502a6780b1b2baf1073d29d075a81298b
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-26 00:19:17 +0300

    Update Book.java

--- MiniBookStore/src/Book.java
+++ MiniBookStore/src/Book.java
@@ -2,11 +2,29 @@ public class Book extends Product {
     //book ozl: product ozl + author, publisher
 
 
-    private String author;
+    private String authorName;
     private String publisher;
 
 
-    public Book(int id, String name, String price, int stock, String author, String publisher) {
+    public Book(int id, String name, String price, int stock, String authorName, String publisher) {
         super(id, name, price, stock);
+        this.authorName = authorName;
+        this.publisher = publisher;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
     }
 }
