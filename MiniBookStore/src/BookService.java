9d6868499daf3f8b643380c855f7203acbd299bc
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-26 00:29:21 +0300

    Create BookService.java

---
+++ MiniBookStore/src/BookService.java
@@ -0,0 +1,28 @@
+public class BookService implements ProductService {
+    @Override
+    public void productMenu() {
+        
+    }
+
+    @Override
+    public void listProduct() {
+
+    }
+
+    @Override
+    public void addProduct() {
+
+    }
+
+    @Override
+    public void removeProduct() {
+
+    }
+
+    @Override
+    public void filterProduct(String filter) {
+
+    }
+    //2.Adim : ProductService i implement et ve methodlari book a uygun sekilde doldur
+
+}
