bf62fcc0a9301cc66521c9437ae1d7352f170531
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-26 00:02:04 +0300

    Update MiniBoookStore.java

--- MiniBookStore/src/MiniBookStore.java
+++ MiniBookStore/src/MiniBookStore.java
@@ -1,3 +1,19 @@
 public class MiniBookStore {
-    
+/*
+Proje: Mini Book Story
+       Online bir kitap market icin urun yonetim uygulamasi yapiniz.
+       Kitap markette kitap ve defter satisi olacak. Ancak ileride yeni urun cesidi eklenebilir olmali
+
+       Kitap ozellikleri : id, isim, birim fiyet, stock, yazar adi, yayinevi
+       Defter ozellikleri : id, isim, birim fiyet, stock, marka, yaprak sayisi
+
+       Kullanici ilgili kategorideki urunleri listeleyebilmeli
+       Kullanici kategoriye gore urun ekleyebilmeli
+       Kullanici urunleri benzersiz numaralarina gore silebilmeli
+       Kullanici urunleri marka(kitap) veya yayinevine gore filtreleyip listeleyebilmeli
+
+*/
+
+
+
 }
