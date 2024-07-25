d54f7740beb3a1fd4131ffbe09a06144b4c39e76
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-26 00:16:44 +0300

    Summary

--- MiniBookStore/src/Book.java
+++ MiniBookStore/src/Book.java
@@ -1,2 +1,12 @@
-public class Book {
+public class Book extends Product {
+    //book ozl: product ozl + author, publisher
+
+
+    private String author;
+    private String publisher;
+
+
+    public Book(int id, String name, String price, int stock, String author, String publisher) {
+        super(id, name, price, stock);
+    }
 }
--- MiniBookStore/src/Notebook.java
+++ MiniBookStore/src/Notebook.java
@@ -1,2 +1,10 @@
-public class Notebook {
+public class Notebook extends Product {
+    //notebook ozl: product ozl + brand, sheet
+
+    private String brand;
+    private String sheet;
+
+    public Notebook(int id, String name, String price, int stock, String brand, String sheet) {
+        super(id, name, price, stock);
+    }
 }
