55139dbea328cd2dc976ee5c26ef486256611364
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-07-28 13:32:43 +0300

    Update MiniBookStore.java

--- MiniBookStore/src/MiniBookStore.java
+++ MiniBookStore/src/MiniBookStore.java
@@ -19,36 +19,13 @@ Proje: Mini Book Story
 
     public static void main(String[] args) {
         enter();
-
-        List<Integer> arr2= new ArrayList<>();
-        arr2.add(5);
-        arr2.add(5);
-        arr2.add(8);
-        arr2.add(7);
-        arr2.add(5);
-
-        List<Integer> arr3= new ArrayList<>();
-        arr3.add(6);
-        arr3.add(6);
-        arr3.add(6);
-        arr3.add(6);
-        arr3.add(6);
-
-        List<List<Integer>> arr = new ArrayList<>();
-        arr.add(0,arr2);
-        arr.add(1,arr3);
-
-
-
-
-        System.out.println(arr.get(0).get(2));
-        System.out.println("Math.abs(5-7) = " + Math.abs(5 - 7));
     }
 
     //1. adim : product, book ve notebook classlari olustur
 
 
     private static void enter() {
+        //
     }
 
 
